97ccff6dcf0c7b96a31965a5f2b64169ca5727d5
Kirill Shurygin <Kirill Shurygin>
2025-01-28 12:54:47 +0300

    add metric annotation to controllers methods

--- src/main/java/ru/t1/java/demo/controller/AccountController.java
+++ src/main/java/ru/t1/java/demo/controller/AccountController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.t1.java.demo.aop.annotation.Metric;
 import ru.t1.java.demo.dto.AccountDto;
 import ru.t1.java.demo.model.Account;
 import ru.t1.java.demo.service.AccountService;
@@ -17,6 +18,7 @@ import java.util.stream.Collectors;
 public class AccountController {
     private final AccountService accountService;
 
+    @Metric
     @GetMapping
     public ResponseEntity<List<AccountDto>> getAllAccounts() {
         List<AccountDto> accounts = accountService.getAllAccounts()
@@ -26,6 +28,7 @@ public class AccountController {
         return new ResponseEntity<>(accounts, HttpStatus.OK);
     }
 
+    @Metric
     @PostMapping
     public ResponseEntity<AccountDto> createAccount(@RequestBody AccountDto account) {
         Account newAccount = accountService.toEntity(account);
@@ -34,6 +37,7 @@ public class AccountController {
         return new ResponseEntity<>(accountService.toDto(savedAccount), HttpStatus.CREATED);
     }
 
+    @Metric
     @PutMapping("/{id}")
     public ResponseEntity<AccountDto> updateAccount(@PathVariable Long id, @RequestBody AccountDto account) {
         if (accountService.getAccountById(id).isEmpty()){
@@ -45,6 +49,7 @@ public class AccountController {
         return new ResponseEntity<>(accountService.toDto(updatedAccount), HttpStatus.OK);
     }
 
+    @Metric
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteAccount(@PathVariable Long id) {
         accountService.deleteAccountById(id);
